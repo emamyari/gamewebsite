13ee054ebe973bf73bcc015aa6864e9be91b011d
emamyari <emamyari@yahoo.com>
2023-12-07 17:21:51 +0330

    add digikala crawler .

--- myapp/functions.py
+++ myapp/functions.py
@@ -1,6 +1,13 @@
+import json
+
+import requests
 from django.shortcuts import render
+import http.client
+
 
 def page2(request):
-    data={'user':['ali','goli','mohammad','vali']}
-    a=render(request,'page2.html',context=data)
+
+    data=requests.get('https://api.digikala.com/v1/promotions/plp_63247410/?has_selling_stock=1&sort=7&seo_url=&page=2').text
+    d=json.loads(data)
+    a=render(request,'page2.html',d)
     return a
